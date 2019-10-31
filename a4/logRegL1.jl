d708aae24c565e0c2d7916d9605d51d1b39aaa74
Alec Xu <Alec_Xu@hotmail.com>
2019-10-30 19:17:45 -0700

    should be done as have low validation error, but did not converge after 100 iterations.

--- a4/logRegL1.jl
+++ a4/logRegL1.jl
@@ -3,7 +3,7 @@ include("misc.jl")
 include("findMin.jl")
 
 # Fits a logistic regression model with L2-regularization
-function logRegL2(X,y,lambda)
+function logRegL1(X,y,lambda)
 
 	(n,d) = size(X)
 
@@ -11,10 +11,10 @@ function logRegL2(X,y,lambda)
 	w = zeros(d,1)
 
 	# Function we're going to minimize (and that computes gradient)
-	funObj(w) = logisticL2Obj(w,X,y,lambda)
+	funObj(w) = logisticL1Obj(w,X,y,lambda)
 
 	# Solve least squares problem
-	w = findMin(funObj,w,derivativeCheck=true)
+	w = findMinL1(funObj,w, lambda)
 
 	# Make linear prediction function
 	predict(Xhat) = sign.(Xhat*w)
@@ -23,9 +23,9 @@ function logRegL2(X,y,lambda)
 	return LinearModel(predict,w)
 end
 
-function logisticL2Obj(w,X,y,lambda)
+function logisticL1Obj(w,X,y,lambda)
 	yXw = y.*(X*w)
-	f = sum(log.(1 .+ exp.(-yXw))) + ((lambda/2)*(w'*w))[1]
-	g = (-X'*(y./(1 .+ exp.(yXw)))) + (lambda * w)
+	f = sum(log.(1 .+ exp.(-yXw))) + lambda*sum(w)
+	g = (-X'*(y./(1 .+ exp.(yXw))))
 	return (f,g)
 end
