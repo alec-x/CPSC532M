e7296a828cdcd516f384357280298930653ea93d
Joyce <jteodoro@alumni.ubc.ca>
2019-11-01 15:24:45 -0700

    Added derivative calculation to softMaxObj function in q3_3

--- a4/q3_3.jl
+++ a4/q3_3.jl
@@ -6,6 +6,8 @@ using JLD
 data = load("multiData.jld")
 (X,y,Xtest,ytest) = (data["X"],data["y"],data["Xtest"],data["ytest"])
 
+softMaxClassifier(X,y)
+
 function softMaxClassifier(X,y)
 
 	(n,d) = size(X)
@@ -22,8 +24,16 @@ function softMaxObj(w,X,y)
 	k = trunc(Int,size(w)[1]/d) # number of classes
 	wNew = reshape(w, k, d)
 
+	gradMatrix = zeros(k,d)
+
 	for c in 1:k
-		for j in 1:ds
-		wNew[c,j] = (y .== c).*(-X[:,c]) + #...
+		for j in 1:d
+			for i in 1:n
+				g +=  ((y .== c)[i] + (1 / sum(exp.((X*wNew')[i,:]))) * (exp((X*wNew')[i,c]))) * X[i,j]
+			end
+			gradMatrix[c,j] = g
+		end
 	end
+
+	return(f,g)
 end
