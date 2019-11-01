493b77f9f57fa6c7b576c039edfaa522264d636a
Joyce <jteodoro@alumni.ubc.ca>
2019-11-01 15:57:28 -0700

    Added loss function, f to softMaxObj function. Reshaped g to be a dkx1 vector.

--- a4/q3_3.jl
+++ a4/q3_3.jl
@@ -26,14 +26,22 @@ function softMaxObj(w,X,y)
 
 	gradMatrix = zeros(k,d)
 
+	f = sum(log.(1 .+ exp.(-yXw)))
+	for i in 1:n
+		f += (-1)*(X*wNew')[i,y[i]] + log(sum(exp.((X*wNew')[i,:])))
+	end
+
+	# Calculates the kxd matrix of partial derivatives
 	for c in 1:k
 		for j in 1:d
 			for i in 1:n
-				g +=  ((y .== c)[i] + (1 / sum(exp.((X*wNew')[i,:]))) * (exp((X*wNew')[i,c]))) * X[i,j]
+				g += ((-1)*(y .== c)[i] + ((1 / sum(exp.((X*wNew')[i,:]))) * (exp((X*wNew')[i,c])))) * X[i,j]
 			end
 			gradMatrix[c,j] = g
 		end
 	end
+
+	g = reshape(g, d*k, 1) # Reshape g from kxd to dkx1 for use in findMin
 
 	return(f,g)
 end
