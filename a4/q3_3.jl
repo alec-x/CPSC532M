32a995bb77ac548ebdbbe37d6ad1ccad6a47bf7b
Joyce <jteodoro@alumni.ubc.ca>
2019-11-01 16:18:22 -0700

    Minor (additional) changes to softMaxobj function

--- a4/q3_3.jl
+++ a4/q3_3.jl
@@ -13,7 +13,7 @@ function softMaxClassifier(X,y)
 	(n,d) = size(X)
 	k = size(unique(y))[1]
 	# Initial guess
-	w = zeros(k*d)
+	w = zeros(500)
 
 	funObj(w) = softMaxObj(w,X,y)
 	w = findMin(funObj,w,verbose=false, derivativeCheck=true)
@@ -23,25 +23,25 @@ function softMaxObj(w,X,y)
 	(n,d) = size(X)
 	k = trunc(Int,size(w)[1]/d) # number of classes
 	wNew = reshape(w, k, d)
-
+	f = 0
+	g = 0
 	gradMatrix = zeros(k,d)
 
-	f = sum(log.(1 .+ exp.(-yXw)))
 	for i in 1:n
-		f += (-1)*(X*wNew')[i,y[i]] + log(sum(exp.((X*wNew')[i,:])))
+		global f += ((-1)*(X*wNew')[i,y[i]] + log(sum(exp.((X*wNew')[i,:]))))[1]
 	end
 
 	# Calculates the kxd matrix of partial derivatives
 	for c in 1:k
 		for j in 1:d
 			for i in 1:n
-				g += ((-1)*(y .== c)[i] + ((1 / sum(exp.((X*wNew')[i,:]))) * (exp((X*wNew')[i,c])))) * X[i,j]
+				global g += ((-1)*(y .== c)[i] + ((1 / sum(exp.((X*wNew')[i,:]))) * (exp((X*wNew')[i,c])))) * X[i,j]
 			end
 			gradMatrix[c,j] = g
 		end
 	end
 
-	g = reshape(g, d*k, 1) # Reshape g from kxd to dkx1 for use in findMin
+	g = reshape(gradMatrix, d*k, 1) # Reshape g from kxd to dkx1 for use in findMin
 
 	return(f,g)
 end
