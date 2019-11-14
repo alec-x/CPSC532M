42c85cee7849aefb3b78ba564c66a5128e87ad56
Alec Xu <Alec_Xu@hotmail.com>
2019-11-14 09:42:33 -0800

    q1_2 works! when lambda greater than 1e-5, starts to behave weird

--- a5/q1_2.jl
+++ a5/q1_2.jl
@@ -5,7 +5,7 @@ function leastSquaresKernelBasis(X,y,lambda,p)
 	n = size(X,1)
     u = (polyKernel(X,X,p) + lambda*I)^-1*y
 	# Make linear prediction function
-	predict(Xhat) = polyKernel(X, Xhat,p)*u
+	predict(Xhat) = polyKernel(Xhat, X,p)*u
 
 	# Return model
 	return LinearModel(predict, u)
@@ -13,10 +13,12 @@ end
 
 function polyKernel(X1,X2, p)
 	# x1_i * x2_j, should be 1xn * nx1 for each element
-	n = size(X,1)
-	K = zeros(n,n)
-	for i in 1:n
-		for j in 1:n
+	n1 = size(X1,1)
+	n2 = size(X2,1)
+
+	K = zeros(n1,n2)
+	for i in 1:n1
+		for j in 1:n2
 			K[i,j] = (1 + X1[i,:]'*X2[j,:])^p
 		end
 	end
